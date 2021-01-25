ced5019bc5d933900970901b9500f46fc3c89a0b
MuXiangChen <loxluthor@gmail.com>
2021-01-26 02:16:26 +0800

    change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,4 @@
 module.exports = {
-    head: [
-        ['link', { rel: 'icon', type: "image/png", sizes: '48x48', href: '/fav/icon-48.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '96x96', href: '/fav/icon-96.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '144x144', href: '/fav/icon-144.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '192x192', href: '/fav/icon-192.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '256x256', href: '/fav/icon-256.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '384x384', href: '/fav/icon-384.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: '512x512', href: '/fav/icon-512.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '57x57', href: '/fav/apple-touch-icon-57.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '60x60', href: '/fav/apple-touch-icon-60.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '72x72', href: '/fav/apple-touch-icon-72.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/fav/apple-touch-icon-76.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '114x114', href: '/fav/apple-touch-icon-114.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/fav/apple-touch-icon-120.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/fav/apple-touch-icon-152.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '167x167', href: '/fav/apple-touch-icon-167.png' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/fav/apple-touch-icon-180.png' }],
-        ['link', { rel: 'mask-icon', href: '/fav/safari-pinned-tab.svg', color: '#0078df' }],
-        ['link', { rel: 'manifest', href: '/fav/manifest.json' }]
-        ['meta', { name: 'theme-color', content: '#0078df' }]
-    ],
     themeConfig: {
         logo: '/favicon.png', // 导航栏 logo
         sidebar: 'auto',
