9bb058b03a12a7a3c917fceda0e87225b1c9433f
MuXiangChen <loxluthor@gmail.com>
2021-02-18 01:07:44 +0800

    new

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,14 +7,44 @@ module.exports = {
         }
     },
     plugins: [
-        '@maginapp/vuepress-plugin-katex',
-        {
-            delimiters: 'dollars'
-        }
+        [
+            '@maginapp/vuepress-plugin-katex',
+            {
+                delimiters: 'dollars'
+            }
+        ],
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'G-Y89QRC8ZG9'
+            }
+        ]
     ],
     head: [
         ['meta', { name: 'theme-color', content: '#fc46aa' }],
         ['link', { rel: 'icon', herf: '/navicon.png' }],
+        ['script', {}, `
+            // var _hmt = _hmt || [];
+            // (function() {
+            // var hm = document.createElement("script");
+            // hm.src = "https://hm.baidu.com/hm.js?**********************";
+            // var s = document.getElementsByTagName("script")[0]; 
+            // s.parentNode.insertBefore(hm, s);
+
+            // 引入谷歌,不需要可删除这段
+            var hm1 = document.createElement("script");
+            hm1.src = "https://www.googletagmanager.com/gtag/js?id=UA-00000000-1";
+            var s1 = document.getElementsByTagName("script")[0]; 
+            s1.parentNode.insertBefore(hm1, s1);
+            })();
+
+            // 谷歌加载,不需要可删除
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Y89QRC8ZG9');
+        `]
 
     ],
     themeConfig: {
